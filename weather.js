7be31dc40daf391095b795a54adbe4efa6d17dd5
Harold Garbeil <hg@Harolds-MacBook-Pro.local>
2023-04-28 13:27:06 -1000

    weather updated

--- weather.js
+++ weather.js
@@ -1,10 +1,42 @@
-var apikey = '43a6eb084cbbd5e39c194ae6de550801' ;
-var topweatherEl = document.getElementById('top-weather');
-console.log(topweatherEl);
-console.log("In weather") ;
+const apikey = '43a6eb084cbbd5e39c194ae6de550801' ;
+const topweatherEl = document.getElementById('top-weather');
+const selectEl = document.querySelector('.select-city') ;
+
+
 var latitude = 64.15 ;
 var longitude = -21.94 ;
 var data = '' ;
+var towns=[] ;
+
+var selectHTML = function loadSelect(){
+
+    
+    $ajaxUtils.sendGetRequest ('data/itin.txt', function(responseText){
+        console.log(responseText);
+        var lines = responseText.split('\n');
+        
+        for (var icity=1 ; icity<lines.length; icity++){
+            console.log(lines[icity]);
+            var cells = lines[icity].split(',');
+            let town={
+                "id": cells[0],
+                "name": cells[2],
+                "lat":cells[4],
+                "lon":cells[5]
+                
+            };
+            towns.push(town);
+            var selEl = document.createElement('option');
+            selEl.value= cells[0] ;
+            selEl.text=cells[2];
+            console.log(selEl);
+            selectEl.appendChild(selEl);
+            selectEl.value = 1 ;
+            console.log(icity);
+            
+        };
+    },false);
+}
 
 var updateWeather = function getWeather (latitude, longitude){
     // var loc = navigator.geolocation.getCurrentPosition((success)=>{
@@ -27,8 +59,26 @@ function showWeatherData (data){
     let {humidity, pressure, sunrise, wind_speed, temp} = data.current ;     
     const curdesc = data.current.weather[0].description ;
 
+    // var selecthtml = `<div class="weather-select">
+    // <label>Weather for : </label>
+    // <select class="select-city" >`;
+
+    
+
+
     topweatherEl.innerHTML =
-    `<div class="weather-item">
+    
+    // <div class="weather-select">
+    //     <label>Weather for : </label>
+    //     <select class="select-city" >
+
+    //     </select> 
+    //     <input type="Submit" value="Update"> 
+
+    // </div>
+    `
+    <br>
+    <div class="weather-item">
     <div>Currently: </div>
     <div>${curdesc}</div>
     </div>
@@ -59,6 +109,18 @@ function showWeatherData (data){
     // </div>` ;
 
 }
+function newcity () {
+    let index = selectEl.value ;
+    // console.log ("index is "+index);
+    // console.log ("town arr lat is "+townArr[index].lat);
+    townLat= towns[index-1].lat ;
+    townLon = towns[index-1].lon ;
+    console.log("townLat is ",townLat);
+    updateWeather (townLat, townLon) ;
+
+}
+
+selectHTML() ;
 updateWeather(latitude,longitude);
 
 
